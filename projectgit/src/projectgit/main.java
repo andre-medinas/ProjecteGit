4eafdd944b585fea608119483b88ef93dcaa86b7
Usuario <Usuario@HP>
2026-03-03 12:51:42 +0100

    1a modificació

--- projectgit/src/projectgit/main.java
+++ projectgit/src/projectgit/main.java
@@ -2,9 +2,10 @@ package projectgit;
 
 public class main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+    public static void main(String[] args) {
+        System.out.println("Versió inicial");
+        System.out.println("1a modificació Git");
+    }
+}
 
 }
