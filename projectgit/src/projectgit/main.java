a1e6dc5d071d3d3b108b9b579075b7d12475b460
andre-medinas <amedinas@insgabrielamistral.cat>
2026-03-03 13:35:35 +0100

    Update main.java

--- projectgit/src/projectgit/main.java
+++ projectgit/src/projectgit/main.java
@@ -5,7 +5,9 @@ public class main {
     public static void main(String[] args) {
         System.out.println("Versió inicial");
         System.out.println("1a modificació Git");
+        System.out.println("2a modificació GitHub");
     }
 }
 
 }
+
